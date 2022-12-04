7dad129a7b459eb988f5fcd91a94496a1b5fd875
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-12-04 22:39:57 +0100

    Reset password

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { JSXElementConstructor, ReactElement } from 'react';
 import { useRoutes } from 'react-router-dom';
 import AuthTabs from './pages/auth/auth-tabs/AuthTabs';
 import ForgotPassword from './pages/auth/forgot-password/ForgotPassword';
+import ResetPassword from './pages/auth/reset-password/ResetPassword';
 
 export default function AppRouter(): ReactElement<
   any,
@@ -15,6 +16,10 @@ export default function AppRouter(): ReactElement<
     {
       path: '/forgot-password',
       element: <ForgotPassword />,
+    },
+    {
+      path: '/reset-password',
+      element: <ResetPassword />,
     },
   ]);
 
