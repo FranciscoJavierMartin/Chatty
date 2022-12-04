2d2eb554455b8a99bcd4aad83e3afad60b66656a
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-12-04 19:52:07 +0100

    Show auth tabs

---
+++ src/pages/auth/auth-tabs/AuthTabs.tsx
@@ -0,0 +1,26 @@
+import './AuthTabs.scss';
+
+export default function AuthTabs(): JSX.Element {
+  return (
+    <>
+      <div className="container-wrapper">
+        <div className="environment">DEV</div>
+        <div className="container-wrapper-auth">
+          <div className="tabs">
+            <div className="tabs-auth">
+              <ul className="tab-group">
+                <li className="tab active">
+                  <button className="login">Sign In</button>
+                </li>
+                <li className="tab">
+                  <button className="signup">Sign Up</button>
+                </li>
+              </ul>
+              <div className="tab-item">login component</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/routes.tsx
@@ -0,0 +1,17 @@
+import React, { JSXElementConstructor, ReactElement } from 'react';
+import { useRoutes } from 'react-router-dom';
+import AuthTabs from './pages/auth/auth-tabs/AuthTabs';
+
+export default function AppRouter(): ReactElement<
+  any,
+  string | JSXElementConstructor<any>
+> | null {
+  const elements = useRoutes([
+    {
+      path: '',
+      element: <AuthTabs />,
+    },
+  ]);
+
+  return elements;
+}
