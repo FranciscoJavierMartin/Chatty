1f0635d050fb8692e056b6e84c4dc51f3b52e9d9
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-12-04 11:50:42 +0100

    Fix variables import

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3110,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "./src/styles/variables.scss";',
+      },
+    },
   },
   plugins: [react()],
 });
