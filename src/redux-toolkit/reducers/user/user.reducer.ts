32a153657239db59d5514c7ece940fc3233f77b3
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-12-11 22:26:28 +0100

    Setup user reducer

---
+++ src/redux-toolkit/reducers/user/user.reducer.ts
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  token: '',
+  profile: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    addUser: (state, action) => {
+      const { token, profile } = action.payload;
+      state.token = token;
+      state.profile = profile;
+    },
+  },
+});
+
+export const { addUser } = userSlice.actions;
+export default userSlice.reducer;
